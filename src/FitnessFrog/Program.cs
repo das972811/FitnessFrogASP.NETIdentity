f7c3601ef811eb2906f3c825e39f0ef3c2fc1e35
das972811 <salasdiego54@gmail.com>
2024-02-01 23:25:22 -0600

    Added the mysql database

--- src/FitnessFrog/Program.cs
+++ src/FitnessFrog/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+using FitnessFrogDb;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Default connection string is null.");
+
+builder.Services.AddDbContext<FitnessFrogContext>(options => options
+    .UseMySQL(connectionString, x => x.MigrationsAssembly("FitnessFrogDb"))
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
